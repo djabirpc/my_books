f20fdd9d951379a0991290673afea1240786e9d5
djabirpc <djabir.pc@outlook.fr>
2021-08-01 04:24:37 +0100

    One-To-Many,Many-To-Many And Deleting

---
+++ my-books/Controllers/PublishersController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using my_books.Data.Services;
+using my_books.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublishersController : ControllerBase
+    {
+        public PublishersService _publishersService;
+
+        public PublishersController(PublishersService publishersService)
+        {
+            _publishersService = publishersService;
+        }
+
+        [HttpPost("add-publisher")]
+        public IActionResult AddPublisher([FromBody] PublisherVM publisher)
+        {
+            _publishersService.AddPublisher(publisher);
+            return Ok();
+        }
+
+        [HttpGet("get-publisher-books-with-authors/{id}")]
+        public IActionResult GetPublisherData(int id)
+        {
+            var _response = _publishersService.GetPublisherData(id);
+            return Ok(_response);
+        }
+
+        [HttpDelete("delete-publisher-by-id/{id}")]
+        public IActionResult DeletePublisherById(int id)
+        {
+            _publishersService.DeletePublisherById(id);
+
+            return Ok();
+        }
+    }
+}
